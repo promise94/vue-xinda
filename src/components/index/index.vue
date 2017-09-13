e7e67d355b05bbd29d2b2b032b2509989ece7973
李伟鹏 <1104898767@qq.com>
2017-09-13 14:36:50 +0800

    laqu

---
+++ src/components/index/index.vue
@@ -0,0 +1,47 @@
+<template>
+    <div id="lunbo">
+        <div class="daohang">
+            <div>
+                <i class="xd xd-shui"></i>
+                <!-- <a href="">财税服务</a> -->
+                <div class="nicai">
+                    <a href="">财税服务</a><br>
+                    <a href="">代理记账</a>
+                    <a href="">税务代办</a><br>
+                    <a href="">审计报告</a>
+                </div>
+            </div>
+            <div>
+                <i class="xd xd-dendrogram"></i>
+                <a href="">财税服务</a>
+                <div>
+
+                </div>
+            </div>
+            <div>
+                <i class="xd xd-zhishichanquan-copy"></i>
+                <a href="">财税服务</a>
+                <div>
+
+                </div>
+            </div>
+            <div>
+                <i class="xd xd-shebao"></i>
+                <a href="">财税服务</a>
+                <div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+@import '../../common/less/index/index.less';
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,16 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 
-
+import index from '../components/index/index'
 Vue.use(Router);
 
 export default new Router({
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'Hello',
-    //   component: Hello
-    // },
+
+    {
+      path:'/index',
+      name:'index',
+      component: index,
+    }
   ]
 })
