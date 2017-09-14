8b60df289f491007ebf44304d47ea213dc32c0c8
sunxiaowei123 <1421439912@qq.com>
2017-09-14 18:55:58 +0800

    财税服务表单

--- src/components/list/services.vue
+++ src/components/list/services.vue
@@ -6,30 +6,37 @@
             </div>
             <div class="content-center">
                 <div class="search">
-                    <ul class="search-one">
-                        <h4>服务分类</h4>
-                        <li>公司注册</li>
-                        <li>注册变更</li>
-                    </ul>
-                    <ul class="search-two">
-                        <h4>类型</h4>
-                        <li>分公司注册</li>
-                        <li>公司注册地址</li>
-                        <li>合伙企业注册</li>
-                        <li>外商独资公司注册</li>
-                        <li>VIE架构</li>
-                        <li>股份公司注册</li>
-                        <li>有限责任公司注册</li><br/>
-                        <li>一般纳税注册地址</li>
-                    </ul>
-                    <ul class="search-one">
-                        <h4>服务区域</h4>
-                    </ul>
+                    <div class="">
+                        <h3>服务分类</h3>
+                        <ul class="search-one">
+                            <li>公司注册</li>
+                            <li>注册变更</li>
+                        </ul>
+                    </div>
+                    <div class="search-two">
+                        <h3>类型</h3>
+                        <ul>
+                            <li>分公司注册</li>
+                            <li>公司注册地址</li>
+                            <li>合伙企业注册</li>
+                            <li>外商独资公司注册</li>
+                            <li>VIE架构</li>
+                            <li>股份公司注册</li>
+                            <li>有限责任公司注册</li><br/>
+                            <li>一般纳税注册地址</li>
+                        </ul>
+                    </div>
+                    <div class="">
+                        <h3>服务区域</h3>
+                        <ul class="search-one">
+
+                        </ul>
+                    </div>
                 </div>
             </div>
             <div class="content-bottom">
                 <div class="">
-                    
+
                 </div>
             </div>
         </div>
@@ -64,47 +71,43 @@ export default {
             margin-top: 24px;
             .search {
                 height: 162px;
-                >ul {
-                    text-align: center;
-                }
-                .search-one {
-                    height: 40px;
-                    width: 948px;
+                >div {
                     display: flex;
-                    border: 1px solid #ccc;
-                    >h4 {
+                    >h3 {
                         width: 98px;
+                        height: 40px;
+                        text-align: center;
                         line-height: 40px;
-                        border-right: 1px solid #ccc;
+                        border-right:0;
+                        border: 1px solid #ccc;
                     }
-                    >li {
-                        width: 200px;
-                        line-height: 40px;
-                        margin: 0 15px;
+                    >ul {
+                        display: flex;
+                        width: 849px;
+                        height: 40px;
+                        border: 1px solid #ccc;
+                        >li {
+                            margin: 12px 10px;
+                        }
                     }
                 }
-            }
-            .search-two {
-                height: 80px;
-                width: 948px;
-                display: flex;
-                border: 1px solid #ccc;
-                >h4 {
-                    width: 98px;
-                    line-height: 80px;
-                    text-align: center;
-                    border-right: 1px solid #ccc;
-                }
-                >li {
-                    width: 200px;
-                    margin: 21px 2px;
-                    background-color: red;
+                .search-two {
+                    >h3 {
+                        height: 80px;
+                         text-align: center;
+                        line-height: 80px;
+                        border-right:0;
+                    }
+                    >ul {
+                        height: 80px;
+                        line-height:56px;
+                    }
                 }
             }
         }
-        .content-bottom{
-            width:948px;
-            height:468px;
+        .content-bottom {
+            width: 948px;
+            height: 468px;
             margin-top: 23px;
             background-color: red;
         }
