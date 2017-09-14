30aa4bfec7755369b5515c7dd307a3325d816506
xiaowei9495 <promise9495@163.com>
2017-09-14 17:28:41 +0800

    app.vue 解决less作用域问题

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@ export default {
 }
 </script>
 
-<style lang="less" scoped>
+<style lang="less">
 @import url('./common/less/global/cssreset.less');
 @import url('./common/less/global/base.less');
 
