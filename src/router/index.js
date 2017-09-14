0644730de073852f7d099931df38f822a5ad24c0
刘四震 <214766179@qq.com>
2017-09-14 11:55:44 +0800

    配置axios

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import main from '../view/main.vue';
 import user from '../view/user.vue';
+import us from '../components/us/us.vue';
+import cart from '../components/cart/cart.vue'
 
 
 Vue.use(Router);
@@ -11,7 +13,21 @@ export default new Router({
     {
       path: '/',
       name: 'Main',
-      component: main
+      component: main,
     },
+    // 加盟我们{
+    {
+      path: '/us',
+      name: 'us',
+      component: us
+    },
+    // }
+    // 购物车{
+    {
+      path: '/cart',
+      name: 'cart',
+      component: cart
+    },
+    // }
   ]
 })
