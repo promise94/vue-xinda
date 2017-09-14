7d48a7b370ff095146c17bf3d57201ec8644a8ff
sunxiaowei123 <1421439912@qq.com>
2017-09-14 13:50:53 +0800

    财务税收列表

--- src/router/index.js
+++ src/router/index.js
@@ -2,16 +2,28 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import main from '../view/main.vue';
 import user from '../view/user.vue';
+import services from '../components/list/services.vue'
+import sifco from '../components/list/sifco.vue'
 
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
+    // {
+    //   path: '/',
+    //   name: 'Main',
+    //   omponent: main
+    // },
     {
-      path: '/',
-      name: 'Main',
-      component: main
+      path: '/services',
+      name: 'services',
+      component: services
+    },
+    {
+      path: '/sifco',
+      name: 'sifco',
+      component: sifco
     },
   ]
 })
