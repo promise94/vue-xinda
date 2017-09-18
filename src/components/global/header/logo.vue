4486c3aead8b860c08c9205b5514d3dce40c8e75
xiaowei9495 <promise9495@163.com>
2017-09-18 10:10:40 +0800

    '搜索初步完成'

--- src/components/global/header/logo.vue
+++ src/components/global/header/logo.vue
@@ -5,19 +5,20 @@
                 <img src="../../../common/images/logo.png" alt="">信达
             </a>
             <div>
-                <span>北京</span>
-                <a href="javascript:;">[切换城市]</a>
+                <span>{{activeCity}}</span>
+                <a @click="swichCity" href="javascript:;">[切换城市]</a>
+                <input type="hidden" v-model="regionId">
             </div>
         </div>
         <ul class="search">
             <li>
-                <span class="active">产品</span>
+                <span @click="switchType(1)" :class="{active: typeIn == 1}">产品</span>
                 <span>|</span>
-                <span>服务商</span>
+                <span @click="switchType(2)" :class="{active: typeIn == 2}">服务商</span>
             </li>
             <li>
-                <input type="text" placeholder="搜索您需要的服务或服务商">
-                <button class="xd xd-search"></button>
+                <input type="text" @keyup.enter="search()" v-model="searchName" placeholder="搜索您需要的服务或服务商">
+                <button @click="search()" class="xd xd-search"></button>
             </li>
             <li>
                 <span>热门服务&nbsp;:&nbsp;</span>
@@ -31,15 +32,73 @@
             <span class="xd xd-lianxidianhua"></span>
             <span>010-83421842</span>
         </div>
+        <div v-show="isShowCity" class="allCity">
+            <h2>城市选择</h2>
+            <p>尊敬的用户您好：请选择准确的地址方便我们为您提供更精确的服务！</p>
+            <div class="city">
+                <span v-for="item of allCity" @click="setCity(item)">{{item.name}}</span>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
     name: 'logo',
+    data() {
+        return {
+            typeIn: 1, // 搜索类型切换 1:产品，2:商品
+            searchName: '', // 搜索内容
+            activeCity: '', // 所选城市
+            allCity: '', // 所有城市
+            regionId: '110100', // 所选城市编码
+            isShowCity: false, // 选择城市面板
+        }
+    },
+    created() {
+        if (this.$route.path.indexOf('search') > -1 && sessionStorage.getItem('searchInfo')) {
+            let searchInfo = JSON.parse(sessionStorage.getItem('searchInfo'));
+            this.searchName = searchInfo.keyword;
+            this.typeIn = searchInfo.type;
+        }
+        this.getActiveCity();
+    },
     methods: {
-        goto(url){
+        search() { // 搜索跳转
+            this.$router.push('/search/' + Math.random().toString(16).substr(2));
+            sessionStorage.setItem('searchInfo', JSON.stringify({ keyword: this.searchName, type: this.typeIn, id: this.regionId }));
+        },
+        goto(url) {
             this.$router.push(url);
+        },
+        switchType(n) {
+            this.typeIn = n;
+        },
+        getActiveCity() {
+            this.$http.post('/common/select-region').then((res) => {
+                if (res.status === 1) {
+                    this.activeCity = res.data.name;
+                }
+            });
+        },
+        getAllCity() {
+            this.$http.post('/common/open-region').then((res) => {
+                this.allCity = res.data;
+            })
+        },
+        swichCity() {
+            this.isShowCity = !this.isShowCity;
+            this.getAllCity();
+        },
+        setCity(val) {
+            console.log(val);
+            this.isShowCity = false;
+            this.$http.post('/common/change-region', { regionId: val.id }).then((res) => {
+                if (res.status === 1) {
+                    this.activeCity = val.name;
+                    this.regionId = val.id;
+                }
+            })
         }
     }
 };
@@ -156,15 +215,15 @@ export default {
                     font-size: 12px;
                     color: #c7c7c7;
                 }
-                >span{
+                >span {
                     padding-left: 15px;
                 }
-                >div{
+                >div {
                     display: inline-block;
-                    span{
+                    span {
                         margin-right: 15px;
                         cursor: pointer;
-                        &:hover{
+                        &:hover {
                             color: orange;
                         }
                     }
@@ -184,6 +243,45 @@ export default {
                 padding-left: 15px;
                 font-size: 20px;
                 color: @colora3;
+            }
+        }
+    }
+}
+
+.allCity {
+    position: absolute;
+    left: 50%;
+    margin-left: -30%;
+    top: 40%;
+    width: 60%;
+    height: 80%;
+    background-color: #fefefe;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    z-index: 1111;
+    h2 {
+        font-size: 35px;
+        text-align: center;
+        color: #fff;
+        background-color: rgb(99, 194, 199);
+        padding: 30px 0 15px;
+    }
+    p {
+        padding-bottom: 25px;
+        background-color: rgb(99, 194, 199);
+        text-align: center;
+        color: #fff;
+    }
+    .city {
+        padding: 35px;
+        span {
+            padding: 10px 15px;
+            border: 1px solid #aaa;
+            border-radius: 5px;
+            cursor: pointer;
+            &:hover {
+                background-color: rgb(99, 194, 199);
+                color: #fff;
             }
         }
     }
