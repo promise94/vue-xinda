36aa36e544ef755841c216557c7cd205834721fb
李伟鹏 <1104898767@qq.com>
2017-09-18 20:05:02 +0800

    更改上次上传小错误 index.js import重复

--- src/router/index.js
+++ src/router/index.js
@@ -4,22 +4,16 @@ import Router from 'vue-router';
 import main from '@/view/main.vue'; // 商品展示、会员中心展示页
 import user from '@/view/user.vue'; // 登录、注册、忘记密码展示页
 
-import us from '../view/us/us.vue';
-import cart from '../view/cart/cart.vue'
 // 首页
 import index from '../view/index/index'
 // 商品详情页
 import goods from '../view/goods/goods'
 import search from '@/view/search/search.vue'; // 搜索结果页
-
-import index from '@/view/index/index';
-
 import services from '@/view/list/services.vue';
 import sifco from '@/view/list/sifco.vue';
 import us from '@/view/us/us.vue'; // 加盟我们
 import storeList from '@/view/store/storeList.vue';
 import storeIndex from '@/view/store/storeIndex.vue';
-
 import cart from '@/view/cart/cart.vue'; // 购物车
 import pay from '@/view/pay/pay.vue';// 支付首页
 
@@ -48,48 +42,48 @@ export default new Router({
       {
         path: '/goods',
         name: 'goods',
-        
+
         component: goods,
       },
-    {
-      path: '/services',
-      name: 'services',
-      component: services
-    },
-    {
-      path: '/sifco',
-      name: 'sifco',
-      component: sifco
-    },
-    {
+      {
+        path: '/services',
+        name: 'services',
+        component: services
+      },
+      {
+        path: '/sifco',
+        name: 'sifco',
+        component: sifco
+      },
+      {
 
-      path: '/storeList',
-      name: 'storeList',
-      component: storeList
-    },
-    {
-      path: '/storeIndex',
-      name: 'storeIndex',
-      component: storeIndex
-    },
-    {
-      path: '/us',
-      name: 'us',
-      component: us
-    },
-    {
-      path: '/search/:keyword',
-      name: 'search',
-      component: search
-    }, {
-      path: '/cart',
-      name: 'Cart',
-      component: cart,
-    }, {
-      path: '/pay',
-      name: 'Pay',
-      component: pay
-    }]
+        path: '/storeList',
+        name: 'storeList',
+        component: storeList
+      },
+      {
+        path: '/storeIndex',
+        name: 'storeIndex',
+        component: storeIndex
+      },
+      {
+        path: '/us',
+        name: 'us',
+        component: us
+      },
+      {
+        path: '/search/:keyword',
+        name: 'search',
+        component: search
+      }, {
+        path: '/cart',
+        name: 'Cart',
+        component: cart,
+      }, {
+        path: '/pay',
+        name: 'Pay',
+        component: pay
+      }]
   },
   {
     path: '/user',
