36731f0177da30cd9d1f10e50edf0d8f0da6c1c9
刘四震 <214766179@qq.com>
2017-09-27 10:48:37 +0800

    微调购物车样式

--- src/view/cart/cart.vue
+++ src/view/cart/cart.vue
@@ -22,8 +22,8 @@
                     <p>{{item1.serviceName}}</p>
                     <p>￥{{item1.unitPrice}}</p>
                     <p>
-                        <span @click="less(item1)" >-</span>
-                        <input type="text" @blur="onblur($event ,item1)" :value="item1.buyNum">
+                        <span @click="less(item1)">-</span>
+                        <input type="text" @blur="onblur($event ,item1)"  onkeyup=" if(this.value.length==1){this.value=this.value.replace(/[^1-9]/g, '')}else{this.value=this.value.replace(/\D/g, '')} " onafterpaste="if(this.value.length==1){this.value=this.value.replace(/[^1-9]/g, '')}else{this.value=this.value.replace(/\D/g, '')} " :value="item1.buyNum">
                         <span @click="add(item1)">+</span>
                         <span v-show="show === 0"></span>
                     </p>
@@ -72,6 +72,8 @@
                 <h1>{{modal_info}}</h1>
             </div>
         </modal>
+        <!-- 删除结果 -->
+        <v-alert :type="alert_options.type" :info="alert_options.info" ref="alert1"></v-alert>
     </div>
 </template>
 
@@ -79,10 +81,12 @@
 //引入modal
 import modal from '@/components/global/modal'; //弹出框引入
 import { mapActions } from 'vuex' //vuex的引入
+import vAlert from '@/components/global/alert';
 export default {
     name: 'div_cart',
     components: {
         modal,
+        vAlert,
     },
     created() {
         this.getTuijian();
@@ -102,18 +106,13 @@ export default {
             modal_info: '',
             jieguo: 0,
             caozuo: '', //成功、失败
-            show:1, //遮罩层
+            show: 1, //遮罩层
+            alert_options: { type: 'error', info: '' }, // 提示框设置
         }
     },
     methods: {
         ...mapActions(['cartAction']),
-        // 失焦事件
-        onblur(ev,item1) {
-            this.getZhezhao(0); //遮罩层打开
-            let vall = ev.target.value;
-            item1.buyNum = vall;
-            this.getxiugai(item1)
-        },
+        
         //价格转化
         fmtPrice(p) {
             return (parseFloat(p) * 0.01).toFixed(2);
@@ -136,12 +135,13 @@ export default {
                     data.forEach(function(item) {
                         item.providerImg = 'http://115.182.107.203:8088/xinda/pic/' + item.providerImg;
                         this.counter += item.buyNum;    //总件数
-                        let price = item.marketPrice;
-                        item.marketPrice = this.fmtPrice(price);    //价格处理
-                        this.monytotal += item.totalPrice //总钱数
+                        let price = item.unitPrice;
+                        item.unitPrice = this.fmtPrice(price);    //价格处理
+                        let totalPrice = item.totalPrice;
+                        item.totalPrice = this.fmtPrice(totalPrice);
+                        this.monytotal += item.totalPrice / 1 //总钱数
                     }, this)
                     this.items = res.data;
-                    // console.log(this.counter)
                     this.cartAction(this.counter);
                 } else {
                     this.willshow = 0;
@@ -163,14 +163,9 @@ export default {
                 this.getxiugai(item1);
             } else {
                 this.counter = 1;
-                // alert("数量不能为零！")
-                this.modal_info = '数量不能为零！';
-                this.$refs.name1.confirm().then(() => {
-                    //确定的回掉函数
-                    this.$refs.name1.show = false //自己手动关闭
-                }).catch(() => {
-                    //点击取消的回掉函数
-                })
+                this.alert_options.type = 'error';
+                this.alert_options.info = "数量不能为0";
+                this.$refs.alert1.alert()
             }
         },
         // 修改事件
@@ -181,7 +176,7 @@ export default {
                     num: item1.buyNum
                 }
             ).then((res) => {
-                
+
                 let status = res.status;
                 if (status == 1) {
                     this.getCartlsit();
@@ -197,20 +192,26 @@ export default {
                 // 点击确定按钮的回调处理
                 this.$refs.name1.show = false;//自己手动关闭
                 this.$http.post('/cart/del', { id: item1.serviceId }).then((res) => {
-                    if (res.status == 1) {  //删除成功的操作                   
-                        this.getCartlsit();
-                        // 删除列表的最后一件
-                        var i = this.items.indexOf(item1);
-                        this.items.splice(i, 1);
-                        this.msg--;
-                        this.cartAction(this.items.length);
-                        setTimeout(this.getJeiguo1, 500)
+                    if (res.status == 1) {  //删除成功的操作                 
+                        //删除结果提示 
+                        this.alert_options.type = 'success';
+                        this.alert_options.info = res.msg;
+                        this.$refs.alert1.alert().then(() => {
+                            this.getCartlsit();
+                            this.msg--;
+                            if(this.msg == 0){
+                                this.cartAction(0);
+                            }
+                        });
+                        // // 删除列表的最后一件
+                        // var i = this.items.indexOf(item1);
+                        // this.items.splice(i, 1);
+                        // this.msg--;
+                        // this.cartAction(this.items.length);
                     } else {
                         this.modal_info = res.msg;
                         this.$refs.name1.confirm().then(() => {
-                            this.$refs.name1.show = false; //手动关闭
-                            // this.getCartlsit();   
-                            this.getJeiguo(1, "X操作失败")
+                            this.$refs.name1.show = false; //手动关闭  
                         })
                         console.log(res);
                     }
@@ -220,8 +221,12 @@ export default {
                 // 点击取消按钮的回调处理
             });
         },
-        reduces(arry) {
-            arry.reduce((x, y) => { return x + y; })
+        // 失焦事件
+        onblur(ev, item1) {
+            this.getZhezhao(0); //遮罩层打开
+            let vall = ev.target.value;
+            item1.buyNum = vall;
+            this.getxiugai(item1)
         },
         //去购物
         goShop() {
@@ -239,18 +244,20 @@ export default {
             }).then((res) => {
                 // console.log(res);
                 if (res.status == 1) {
+                    // this.getCartlsit();
                     let dingdan = res.data;
                     this.$router.push({
                         path: '/pay',
                         query: { val: dingdan }
-                    })
+                    });
+                    this.cartAction(0);
                 } else {
                     // this.modal_info = ""
                 }
             })
         },
         //触发遮罩层
-        getZhezhao(n){
+        getZhezhao(n) {
             this.show = n;
         },
         //热门服务获取
@@ -282,11 +289,6 @@ export default {
         },
         //热门-传id、跳转页面
         showDetails(id) {
-            // 跳转到页面顶层
-            // router.afterEach((to, from, next) => {
-            // window.scrollTo(0, 0);
-            // });
-
             this.$router.push({
                 path: '/goods',
                 query: { id }
